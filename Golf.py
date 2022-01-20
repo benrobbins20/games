721a472cf263c3f06211525b092217ab4e232c7a
Benjamin Robbins <benro2211@gmail.com>
2022-01-20 11:37:19 -0700

    need to add error handling for input typos and congrats messages for under par

--- Golf.py
+++ Golf.py
@@ -29,12 +29,16 @@ class Golfbag:
     return bag_dist
   def __repr__(self):
     return ('Bag:\n{}'.format(self.bag_dist))
+  
+  #####################################PLAYER##################################################################
   def player(self,hole):
+    self.putt_counter = 0
     distance_remaining = (hole.length())
     print('You are playing a {} yard par {}.'.format(distance_remaining,hole.par))
     self.shot_number = 0
     while abs(distance_remaining) > 20:
       print('Distance remaining: {} yards'.format(distance_remaining))
+
       club = input('Choose a club from the following.\n\n{}\n:'.format(self.club_distance()))
       club = club.lower()
       club_range = list(self.club_distance()[club])
@@ -42,21 +46,28 @@ class Golfbag:
       print('Your shot went {} yards.'.format(hit))
       distance_remaining-=hit
       distance_remaining = abs(distance_remaining)
-      self.shot_number+=1
+      self.shot_number += 1
     print('{}Congrats you\'re on the green!'.format("\033[31;1;4m"))
     while abs(distance_remaining) > 0:
       print('You have a {} foot putt left.'.format(int(distance_remaining*3,)))
-      putt = random.randint(int(distance_remaining/2),distance_remaining)
+      putt = random.randint(round(distance_remaining*.75,0),distance_remaining)
       print('Your putt went {} feet'.format(putt*3))
       distance_remaining -= putt
       distance_remaining = abs(distance_remaining)
       self.shot_number += 1
+      self.putt_counter += 1
       time.sleep(1)
     
-      
-    print('{}You\'ve finished this hole with {} strokes.'.format('\033[0m',self.shot_number))
+    score = self.shot_number
+    
+
+    print('{}You\'ve finished this hole in {} strokes with {} putts.'.format('\033[0m',score,self.putt_counter))
+  
+  ##############################################PLAYER#########################################################
+  
   def score(self):
-    print(self.shot_number)
+    return self.shot_number
+
 
 
 
@@ -89,14 +100,28 @@ class Hole:
 ############################################PLAY##########################################
 
 class Course:
-  def __init__(self,lst):
-    self.lst = lst 
-    for h in lst:
-      self.hole = h
-      print(self.hole)
+  def __init__(self,bag,lst):
+    par_total = 0
+    score = 0
+    hole_number = 1
+    for hole in lst:
+      print("You are on hole {}".format(hole_number))
+      bag.player(hole)
+      score += bag.score()
+      hole_number += 1
+      par_total += hole.par
+      print('Current score: {}'.format(score))
+    print('Par: {}'.format(par_total))
+    print( '\n\nYou shot {} on a par {} course!'.format(score,par_total))
+    print('Well Done!!')
 
-  def __repr__(self):
-    return self.hole
+
+    
+    
+    
+    
+
+  
 
 
 
@@ -115,36 +140,43 @@ class Course:
 
 #rand_dist = random.randint(pw_list[0],pw_list[-1])
 #print(rand_dist)
+
+
+
+#hole_1 = Hole(random.randint(3,5))
+#bag_1.player(hole_1)
+#print(bag_1.score()) DOES print the score after bag_1.player is called 
+
 def hole_list():
-  hole_1 = Hole(random.randint(3,6))
-  hole_2 = Hole(random.randint(3,6))
-  hole_3 = Hole(random.randint(3,6))
-  hole_4 = Hole(random.randint(3,6))
-  hole_5 = Hole(random.randint(3,6))
-  hole_6 = Hole(random.randint(3,6))
-  hole_7 = Hole(random.randint(3,6))
-  hole_8 = Hole(random.randint(3,6))
-  hole_9 = Hole(random.randint(3,6))
-  hole_10 = Hole(random.randint(3,6))
-  hole_11 = Hole(random.randint(3,6))
-  hole_12 = Hole(random.randint(3,6))
-  hole_13 = Hole(random.randint(3,6))
-  hole_14 = Hole(random.randint(3,6))
-  hole_15 = Hole(random.randint(3,6))
-  hole_16 = Hole(random.randint(3,6))
-  hole_17 = Hole(random.randint(3,6))
-  hole_18 = Hole(random.randint(3,6))
-  return [hole_1,hole_2,hole_3,hole_4,hole_5,hole_6,hole_7,hole_8,hole_9,hole_10,hole_11,hole_12,hole_13,hole_14,hole_15,hole_16,hole_17,hole_18]
+  hole_1 = Hole(random.randint(3,5))
+  hole_2 = Hole(random.randint(3,5))
+  hole_3 = Hole(random.randint(3,5))
+  '''
+  hole_4 = Hole(random.randint(3,5))
+  hole_5 = Hole(random.randint(3,5))
+  hole_6 = Hole(random.randint(3,5))
+  hole_7 = Hole(random.randint(3,5))
+  hole_8 = Hole(random.randint(3,5))
+  hole_9 = Hole(random.randint(3,5))
+  hole_10 = Hole(random.randint(3,5))
+  hole_11 = Hole(random.randint(3,5))
+  hole_12 = Hole(random.randint(3,5))
+  hole_13 = Hole(random.randint(3,5))
+  hole_14 = Hole(random.randint(3,5))
+  hole_15 = Hole(random.randint(3,5))
+  hole_16 = Hole(random.randint(3,5))
+  hole_17 = Hole(random.randint(3,5))
+  hole_18 = Hole(random.randint(3,5))
+  '''
+  return [hole_1,hole_2,hole_3]#,hole_4,hole_5,hole_6,hole_7,hole_8,hole_9,hole_10,hole_11,hole_12,hole_13,hole_14,hole_15,hole_16,hole_17,hole_18]
 c_1 = hole_list()
-#print(c_1)
-
 bag_1 = Golfbag('lob wedge','sand wedge','gap wedge','pitching wedge','9 iron','8 iron','7 iron','6 iron','5 iron','4 iron','3 iron','3 wood','driver')
+course_1 = Course(bag_1,c_1)
 
 
-#course_1 = Course(c_1)
-#print(course_1.hole)
-hole_1 = Hole(5)
-bag_1.player(hole_1)
+
+
+
 
 
 
